eb43d2f5369972d8621988db8f80e39d2dfb68c8
Grigoriy Dobryakov <grigoriy@dobryakov.com>
2015-03-23 20:18:43 +0300

    add paysite filter to rss

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -3,7 +3,12 @@ class ExportController < ApplicationController
   def index
 
     @user = User.find(params[:user_id])
-    @galleries = @user.galleries
+
+    if params[:filter]
+      @galleries = @user.galleries.where(:paysite => params[:filter][:paysite_id])
+    else
+      @galleries = @user.galleries
+    end
 
     respond_to do |format|
       format.rss { render :layout => false }
